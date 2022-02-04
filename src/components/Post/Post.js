934507d79707e5cf07ce13f1c5943256918c9fc4
dhanpalsinghrajpurohit <dhanpal.singh.rajpurohit09@gmail.com>
2022-02-04 17:36:24 +0530

    youtube api working

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,19 +6,21 @@ class Post extends Component{
   
     constructor(props) {
         super(props);
-       
+       this.Message = null;
+       this.authUrl = null;
         this.state={
             Title:null,
             Description:null,
             File:null,
             Tags:"testing",
             Message:null,
-            authUrl:"null",
+            authUrl:'',
         }
         
         this.Title = this.Title.bind(this);  
         this.Description = this.Description.bind(this);  
         this.File = this.File.bind(this);  
+        this.AuthUrl = this.AuthUrl.bind(this);
         this.uploaddata = this.uploaddata.bind(this);
         this.authUser =  this.authUser.bind(this);
       }
@@ -26,7 +28,9 @@ class Post extends Component{
       Title(event) {
         this.setState({ Title: event.target.value })
       } 
-      
+      AuthUrl(value){
+        this.setState({authUrl:value});
+      }
       
       Description(event) {
         this.setState({ Description: event.target.value })
@@ -40,27 +44,15 @@ class Post extends Component{
           
         }
      }
-    authUser(event){
+    authUser = async (event) =>{
       event.preventDefault();
-      console.log("dfakdfj");
-      axios
-        .get('http://localhost:5000/postauth')
-        .then(function (res) {
-          console.log(res);
-            if (res.data.url && res.status==200) {
-              this.state.authUrl = res.data.url;
-              this.setState.authUrl(res.data.url);
-            
-            } 
-            else if (res.status == 401) {
-              alert("Oops! ");
-            }
-          }, function (e) {
-            alert("Error submitting form!");
-          });
+      let res = await axios.get('http://localhost:5000/postauth');
+      this.setState({authUrl:res.data.url});
+      this.setState({Message:res.data.message});
+      console.log(res);
     }
 
-    uploaddata(event){
+    uploaddata = async (event)=>{
         alert("dfjkdf");
         event.preventDefault();
         const file = document.querySelector('input[type="file"]').files[0];
@@ -71,36 +63,43 @@ class Post extends Component{
         // console.log(this.state.File);
         // console.log(this.state.Title);
         // console.log(this.state.Description);
-        axios
-        .post('http://localhost:5000/upload',data)
-        .then(function (res) {
-            if (res.result) {
-              this.setState({Message:res.message});
-            } else if (res.status == 401) {
-              this.setState({Message:res.message});
-            }
-          }, function (e) {
-            this.setState({Message:e});
-          });
+       
+          let res = await axios.post('http://localhost:5000/upload',data);
+          if(res.data.message){
+            this.setState({Message:res.data.message});
+          }else {
+            this.setState({Message:res.data.message});
+          }
      
       }
 
    
     
     callback(event){
-      axios
-        .get('http://localhost:5000/callback')
-        .then(function (res) {
-          console.log(res);
-            if (res.status==200) {
-              console.log("work");
+      // axios
+      //   .get('http://localhost:5000/callback')
+      //   .then(function (res) {
+      //     console.log(res);
+      //       if (res.status==200) {
+      //         console.log("work");
               
-            } else if (res.status == 401) {
-              alert("Oops! ");
-            }
-          }, function (e) {
-            alert("Error submitting form!");
-          });
+      //       } else if (res.status == 401) {
+      //         alert("Oops! ");
+      //       }
+      //     }, function (e) {
+      //       alert("Error submitting form!");
+      //     });
+      let res = axios.get('http://localhost:5000/callback');
+    }
+
+    checkStatus = async (event)=> {
+      let res = await axios.get('http://localhost:5000/checkStatus');
+      if(res.data){
+
+      }
+      this.setState({authUrl:res.data.url});
+      this.setState({Message:res.data.message});
+      console.log(res);
     }
 
     render(){
@@ -109,13 +108,16 @@ class Post extends Component{
         let authButton = null;
         if(this.state.Message){
           alertBox = (<div className="alert alert-primary" role="alert">
-                        This is a primary alert—check it out!
+                        {this.state.Message}
                     </div>);
         }
         if(this.state.authUrl){
           authButton = (
-            <a href={this.state.authUrl} className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+            <a href={this.state.authUrl} className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Click Here!</a>
           );
+        }
+        if(!this.state.authUrl){
+          authButton = (<button className="btn btn-primary" onClick={this.authUser}>Auth</button>)
         }
         return(
             <React.Fragment>
@@ -123,7 +125,7 @@ class Post extends Component{
                     <div className="col-lg-6 offset-lg-3">
                       {alertBox}
                         <div className="row justify-content-center">
-                        <form onSubmit={this.uploaddata} method="POST" encType="multipart/form-data" onLoad={this.authUser}>
+                        <form onSubmit={this.uploaddata} method="POST" encType="multipart/form-data" onLoad={this.checkStatus}>
                             <div className="form-group">
                                 <label for="txt_title">Title</label>
                                 <input type="text" className="form-control" id="txt_title" onChange={this.Title} />
@@ -155,13 +157,12 @@ class Post extends Component{
                                 <textarea className="form-control" id="txt_description" rows="3" onChange={this.Description}></textarea>
                             </div>
                             <button className="btn btn-primary" type="submit">Publish</button>
-
+                             
+                            {/* <button className="btn btn-primary" onClick={this.callback}>callbakc</button> */}
                         </form>
                         
                         </div>
-                        <button className="btn btn-primary" onClick={this.authUser}>Auth</button>
-
-                        <button className="btn btn-primary" onClick={this.callback}>callbakc</button>
+                        
                         {authButton}
                     </div>
                 </div>
